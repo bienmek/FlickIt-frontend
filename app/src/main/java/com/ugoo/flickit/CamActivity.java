e89a0ba1cce11d0b711dd8a258d620233b1a1680
TitouXTv <thibault.velly@gmail.com>
2022-10-17 10:57:56 +0200

    button main -> cam

---
+++ app/src/main/java/com/ugoo/flickit/CamActivity.java
@@ -0,0 +1,14 @@
+package com.ugoo.flickit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+    }
+}
