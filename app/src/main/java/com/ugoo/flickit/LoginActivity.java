fa1d53c329d69478f06d8591f189084e83fc9bf4
Ugo <50956402+bienmek@users.noreply.github.com>
2022-10-18 11:21:38 +0200

    Home page
    Linking the login to the home page

--- app/src/main/java/com/ugoo/flickit/LoginActivity.java
+++ app/src/main/java/com/ugoo/flickit/LoginActivity.java
@@ -2,7 +2,13 @@ package com.ugoo.flickit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView email = (TextView) findViewById(R.id.email);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("email " + email.getText().toString());
+                System.out.println("password " + password.getText().toString());
+                Intent intent = new Intent(getApplicationContext(), com.ugoo.flickit.HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
