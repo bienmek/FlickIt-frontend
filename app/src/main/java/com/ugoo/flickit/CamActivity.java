4de51d72bdfdd5efe44b9ebd02eb6ebf495e5e33
TitouXTv <thibault.velly@gmail.com>
2022-10-20 11:01:50 +0200

    prise de photo fonctionnelle

--- app/src/main/java/com/ugoo/flickit/CamActivity.java
+++ app/src/main/java/com/ugoo/flickit/CamActivity.java
@@ -1,14 +1,110 @@
 package com.ugoo.flickit;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CamActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 1234;
+    private static final int CAPTURE_CODE = 1001;
+    MaterialButton Button;
+    ImageView imageView;
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
+
+        Button = findViewById(R.id.capture_picture_Id);
+        imageView = findViewById(R.id.ImageViewId);
+
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else{
+                        openCamera();
+                    }
+                }
+                else {
+                    openCamera();
+                }
+            }
+        });
+
+
+
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "new image");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent camintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        camintent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(camintent, CAPTURE_CODE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode){
+            case PERMISSION_CODE:
+                if (grantResults.length>0 && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED){
+                    openCamera();
+                }else {
+                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode ==  RESULT_OK){
+             imageView.setImageURI(image_uri);
+        }
     }
 }
+
